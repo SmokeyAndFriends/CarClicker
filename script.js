985abc2516bd3411fcf364f648317c616cae21e0
Ethan Belcher <pandurrerock@gmail.com>
2021-04-11 19:56:37 +0000

    Alpha 5

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const img = document.getElementById('img');
 const clicks = document.getElementById('clicks');
 const am = document.getElementsByClassName('am')[0];
+const amc = am.getElementById('amc')
+const ami = am.getElementById('ami')
 
 var cbx = 0;
+var am$ = 100;
+
+function amActivate() {
+    cbx += cps;
+};
 
 img.addEventListener("click", function() {
     cbx += 1;
@@ -10,6 +17,25 @@ img.addEventListener("click", function() {
 });
 
 am.addEventListener("click", function() {
-    alert('Looks good')
+    price = 100;
+    var yn = prompt('Are you sure? [y|n]')
+    switch(yn) {
+        case('y'): {
+            if(cbx >= am$){
+                cbx -= am$;
+                cps += 0.5 * 1.5;
+                Math.round(cps);
+                am$ += 100 * .20;
+                clicks.textContent = "Carbux: " + cbx;
+                amc.textContent = 'Cost: ' + am$ + 'cbx';
+                ami.textContent =  cps + 'carbux per second'
+            }
+        }
+        case('n'): {
+            return;
+        }
+    }
 });
+
+setInterval(amActivate(), 1000)
 
