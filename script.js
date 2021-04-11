2af34ddf41124131aa0085ceff89a81678be04d5
Ethan Belcher <pandurrerock@gmail.com>
2021-04-11 04:34:27 +0000

    Alpha build 1

---
+++ script.js
@@ -0,0 +1,9 @@
+const img = document.getElementById('clicker');
+const clicks = document.getElementById('clicks')
+
+var cbx = 0;
+
+img.addEventListener("click", function() {
+    cbx += 1;
+    clicks.textContent = "Carbux: " + cbx;
+});
